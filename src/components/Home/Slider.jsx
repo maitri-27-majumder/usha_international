a1d2262389887c7b023577166d0bd9229242e2d4
inosukeLegal <kriti.kalpa@mikelegal.com>
2024-11-11 00:13:11 +0530

    fix: removed and added large file

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/Home/Slider.scss";
 import slider1 from "../../assets/slide1.mp4";
-import slider2 from "../../assets/slide2.mov";
+import slider2 from "../../assets/largeslide.mov";
 
 const Slider = () => {
   const duration = [4, 9];
