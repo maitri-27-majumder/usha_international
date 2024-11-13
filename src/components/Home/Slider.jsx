5710a0c47f00c65a6fc85a3a59f66f126b35b282
inosukeLegal <kriti.kalpa@mikelegal.com>
2024-11-13 23:23:50 +0530

    updated video for slides

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/Home/Slider.scss";
-import slider1 from "../../assets/slide1.mp4";
-import slider2 from "../../assets/slide2.mp4";
 
 const Slider = () => {
   const duration = [4, 9];
-  const slides = [slider1, slider2];
+  const slides = ["/slide1.mp4", "/slide2.mp4"];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -24,30 +22,30 @@ const Slider = () => {
       <div className="slider-content">
         <h1 className="tagline">We Deliver Premium Indian Products Globally</h1>
         <p className="sub-tagline">Your requirements are our priority</p>
-        {currentIndex === 0 && (
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline={true}
-            webkit-playsinline="true"
-            className="slider-bg"
-          >
-            <source src={slider1} type="video/mp4" />
-          </video>
-        )}
-        {currentIndex === 1 && (
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline={true}
-            webkit-playsinline="true"
-            className="slider-bg"
-          >
-            <source src={slider2} type="video/mp4" />
-          </video>
-        )}
+        {/* {currentIndex === 0 && ( */}
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline={true}
+          webkit-playsinline="true"
+          className="slider-bg"
+        >
+          <source src={slides[currentIndex]} type="video/mp4" />
+        </video>
+        {/* )} */}
+        {/* {currentIndex === 1 && (
+        <video
+          autoPlay
+          loop
+          muted
+          playsinline={true}
+          webkit-playsinline="true"
+          className="slider-bg"
+        >
+          <source src={slider2} type="video/mp4" />
+        </video>
+        )} */}
         <div className="slide-mask"></div>
       </div>
     </div>
