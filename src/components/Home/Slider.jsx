bfc2a5151a22137c5e9ce00e9f16bc2d2a85aa7d
inosukeLegal <kriti.kalpa@mikelegal.com>
2024-11-14 00:24:18 +0530

    updated video for slides

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect, useRef } from "react";
 import "../../styles/Home/Slider.scss";
 
 const Slider = () => {
-  const duration = [4, 9];
-  const slides = ["/slide1.mp4", "/slide2.mp4"];
+  const duration = [9, 4];
+  const slides = ["/slide2.mp4", "/slide1.mp4"];
 
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [videoSrc, setVideoSrc] = useState("/slide1.mp4");
+  const [videoSrc, setVideoSrc] = useState("/slide2.mp4");
   const videoRef = useRef();
 
   useEffect(() => {
@@ -30,18 +30,17 @@ const Slider = () => {
         <h1 className="tagline">We Deliver Premium Indian Products Globally</h1>
         <p className="sub-tagline">Your requirements are our priority</p>
         {/* {currentIndex === 0 && ( */}
-        <div className="slider-bg">
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline={true}
-            webkit-playsinline="true"
-            ref={videoRef}
-          >
-            <source src={videoSrc} type="video/mp4" />
-          </video>
-        </div>
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline={true}
+          webkit-playsinline="true"
+          className="slider-bg"
+          ref={videoRef}
+        >
+          <source src={videoSrc} type="video/mp4" />
+        </video>
         {/* )} */}
         {/* {currentIndex === 1 && (
         <video
