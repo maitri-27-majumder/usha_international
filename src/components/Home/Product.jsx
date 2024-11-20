1464d7a37b85ab4d5202ac123ceb8f47546b4482
inosukeLegal <kriti.kalpa@mikelegal.com>
2024-11-20 14:13:41 +0530

    modified 3d images

--- src/components/Home/Product.jsx
+++ src/components/Home/Product.jsx
@@ -9,9 +9,9 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
 const Products = () => {
-  function Model({ path, position }) {
+  function Model({ path, position, scale }) {
     const { scene } = useGLTF(path);
-    return <primitive object={scene} scale={8} position={position} />;
+    return <primitive object={scene} scale={scale} position={position} />;
   }
   const products = [
     {
@@ -20,6 +20,50 @@ const Products = () => {
         "Unlock the vibrant and rich flavors of India with our premium spice collection. Each spice is carefully sourced and handpicked to bring out the best in your culinary creations. From bold chili powders to aromatic cardamoms, our spices are perfect for elevating any dish to new heights of taste and authenticity.",
       image: spices, // Update the path as per your image location
       link: "/products/spices",
+      iframe: (
+        <div className="canvas-container">
+          <Canvas camera={{ position: [6,4, -2], fov: 50 }} style={{ width: "100%", height: "100%" }}>
+          <ambientLight intensity={1} />
+          <directionalLight position={[10, 10, 10]} intensity={2} />
+          <directionalLight position={[-10, -10, -10]} intensity={1} />
+            <OrbitControls
+              minDistance={4}
+              maxDistance={7}
+              enablePan={false}
+            />
+            <Model path={"/spices.glb"} position={[0, -1, 0]} scale={15} />
+          </Canvas>
+        </div>
+      ),
+    },
+    {
+      name: "Tea: A Sip of Tradition",
+      description:
+        "Indulge in the soothing experience of our finest teas, sourced from renowned plantations. Every sip reflects the tradition and care put into crafting each blend. From robust black teas to delicate greens, our collection offers a delightful escape into the world of refreshing and aromatic flavors.",
+      image: tea,
+      link: "/products/tea",
+      iframe: (
+        <div className="canvas-container">
+          <Canvas camera={{ position: [2, 7, 5], fov: 50 }} style={{ width: "100%", height: "100%" }}>
+          <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={2} />
+            {/* <directionalLight position={[-10, -10, -10]} intensity={1} /> */}
+            <OrbitControls
+              minDistance={4}
+              maxDistance={7}
+              enablePan={false}
+            />
+            <Model path={"/tea.glb"} position={[1, 0, 0]} scale={18} />
+          </Canvas>
+        </div>
+      ),
+    },
+    {
+      name: "Handicrafts: Artistry in Every Detail",
+      description:
+        "Our exquisite collection of handicrafts showcases Indias rich artistic traditions. Each piece, from decorative items to functional crafts, is handcrafted with skill and care, making it a perfect addition to homes and businesses.",
+      image: handicrafts,
+      link: "/products/handicrafts",
       iframe: (
         <div className="canvas-container">
           <Canvas camera={{ position: [2, 5, 5], fov: 50 }} style={{ width: "100%", height: "100%" }}>
@@ -31,39 +75,9 @@ const Products = () => {
               maxDistance={7}
               enablePan={false}
             />
-            <Model path={"/handicraft.glb"} position={[0, -2, 0]} />
+            <Model path={"/handicraft.glb"} position={[0, -2, 0]} scale={8} />
           </Canvas>
         </div>
-      ),
-    },
-    // {
-    //   name: "Tea: A Sip of Tradition",
-    //   description:
-    //     "Indulge in the soothing experience of our finest teas, sourced from renowned plantations. Every sip reflects the tradition and care put into crafting each blend. From robust black teas to delicate greens, our collection offers a delightful escape into the world of refreshing and aromatic flavors.",
-    //   image: tea,
-    //   link: "/products/tea",
-    //   iframe: (
-    //     <iframe
-    //       src="https://skybox.blockadelabs.com/e/854c845c5c5e3ee66401c37ba93747ca"
-    //       width="540"
-    //       height="360"
-    //     ></iframe>
-    //   ),
-    // },
-    {
-      name: "Handicrafts: Artistry in Every Detail",
-      description:
-        "Our exquisite collection of handicrafts showcases Indias rich artistic traditions. Each piece, from decorative items to functional crafts, is handcrafted with skill and care, making it a perfect addition to homes and businesses.",
-      image: handicrafts,
-      link: "/products/handicrafts",
-      iframe: (
-        <iframe
-          src="https://lumalabs.ai/embed/0c2354fd-8e6a-402e-9c3c-40b3890b74b5?mode=sparkles&background=%23ffffff&color=%23000000&showTitle=false&loadBg=true&logoPosition=bottom-left&infoPosition=bottom-right&cinematicVideo=undefined&showMenu=false"
-          width="540"
-          height="360"
-          frameBorder="0"
-          title="luma embed"
-        ></iframe>
       ),
     },
     {
@@ -73,13 +87,19 @@ const Products = () => {
       image: furniture,
       link: "/products/furniture",
       iframe: (
-        <iframe
-          src="https://lumalabs.ai/embed/6a1b0c89-15c1-4c7e-a708-7c6e4e2b5e54?mode=sparkles&background=%23ffffff&color=%23000000&showTitle=false&loadBg=true&logoPosition=bottom-left&infoPosition=bottom-right&cinematicVideo=undefined&showMenu=false"
-          width="540"
-          height="360"
-          frameBorder="0"
-          title="luma embed"
-        ></iframe>
+        <div className="canvas-container">
+          <Canvas camera={{ position: [-6, 2, 5], fov: 50 }} style={{ width: "100%", height: "100%" }}>
+          <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={2} />
+            <directionalLight position={[-10, -10, -10]} intensity={1} />
+            <OrbitControls
+              minDistance={4}
+              maxDistance={7}
+              enablePan={false}
+            />
+            <Model path={"/furniture.glb"} position={[0, 0, 0]} scale={4}/>
+          </Canvas>
+        </div>
       ),
     },
     // {
